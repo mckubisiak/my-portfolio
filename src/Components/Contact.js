ebbfdb42c2b8ea4b26d87750578a90cdb0028cf5
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-14 11:37:46 -0700

    update contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,19 +4,32 @@ export default function Contact() {
     return (
         <div >
 
-<form name="contact" method="post">
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
+<form 
+name="contact" 
+method="post"
+data-netlify="true"
+>
+    
+            <p>
+            <label>Your Name: <input 
+            type="text" 
+            name="name"/>
+            </label>
+            </p>
+            <p>
+            <label>Your Email: <input 
+            type="email" 
+            name="email"/>
+            </label>
+            </p>
+            <p>
+            <label>Message: <textarea 
+            name="message">
+                </textarea></label>
+            </p>
+            <p>
             <button type="submit">Send</button>
-          </p>
+            </p>
         </form>
         </div>
     );
