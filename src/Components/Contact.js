0c2ceccd77025f4d284f5cb4b5ab33f232c7f3cb
Kubi Kubisiak <mckubisiak@gmail.com>
2021-07-16 18:35:33 -0700

    resolved formatting and syntax issues for contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,41 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-export default function Contact() {
-    return (
-        <div >
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
-<form 
-name="contact" 
-data-netlify="true"
-netlify
->
-    
+export default class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+    }
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
             <p>
-            <label>Your Name: <input 
-            type="text" 
-            name="name"/>
+            <label>
+                Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
             </label>
             </p>
             <p>
-            <label>Your Email: <input 
-            type="email" 
-            name="email"/>
+            <label>
+                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
             </label>
             </p>
             <p>
-            <label>Message: <textarea 
-            name="message">
-                </textarea></label>
+            <label>
+                Message: <textarea name="message" value={message} onChange={this.handleChange} />
+            </label>
             </p>
             <p>
             <button type="submit">Send</button>
             </p>
         </form>
-        </div>
-    );
+            </div>
+        )
+    }
 }
-
-
-
-
-
